4e7899f60a47a1949695954af14bbeeddfca44dc
Marcos de Melo <marcdmelo@gmail.com>
2024-04-25 11:54:35 -0300

    Primeira aula finalizada

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOCADORA DEV</title>
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>LOCADORA DEV</h1>
+        <ul>
+            <li>
+                <a href="index.php?menu=home">Home</a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-videos">Vídeos</a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-categorias">Categorias</a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-clientes">Clientes</a>
+            </li>
+            <li>
+                <a href="index.php?menu=locacoes">Locações</a>
+            </li>
+        </ul>
+    </header>
+</body>
+</html>
