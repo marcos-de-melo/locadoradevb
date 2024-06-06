91a450fc625e2cdb1913c6567040dc5f4a9e78c8
Marcos de Melo <marcdmelo@gmail.com>
2024-06-06 10:52:42 -0300

    lista videos pronto

--- lista-videos.php
+++ lista-videos.php
@@ -1,1 +1,34 @@
+<?php
+$sql = "SELECT * FROM tbvideos";
+$rs = mysqli_query($conexao,$sql);
+?>
 <h2>Lista de Vídeos</h2>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Duração</th>
+            <th>Valor da Locação</th>
+            <th>Categoria</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+       <?php
+       while($dados = mysqli_fetch_assoc($rs)){
+       ?>
+        <tr>
+            <td>353</td>
+            <td>Crepusculo</td>
+            <td>2:30 hs</td>
+            <td>R$ 10,00</td>
+            <td>Romance</td>
+            <td>Disponivel</td>
+        </tr>
+        <?php
+       }
+        ?>
+    </tbody>
+</table>
